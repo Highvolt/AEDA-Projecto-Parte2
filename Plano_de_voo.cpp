9c0dee9381125fd9ae87fb9d760622e5bf68f989
Pedro Borges <highvolt16@gmail.com>
2010-10-28 16:35:15 +0100

    Added class plano de voo

---
+++ Plano_de_voo.cpp
@@ -0,0 +1,17 @@
+/*
+ * Plano_de_voo.cpp
+ *
+ *  Created on: 28 de Out de 2010
+ *      Author: pedroborges
+ */
+
+#include "Plano_de_voo.h"
+
+Plano_de_voo::Plano_de_voo() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Plano_de_voo::~Plano_de_voo() {
+	// TODO Auto-generated destructor stub
+}
---
+++ Plano_de_voo.h
@@ -0,0 +1,17 @@
+/*
+ * Plano_de_voo.h
+ *
+ *  Created on: 28 de Out de 2010
+ *      Author: pedroborges
+ */
+
+#ifndef PLANO_DE_VOO_H_
+#define PLANO_DE_VOO_H_
+
+class Plano_de_voo {
+public:
+	Plano_de_voo();
+	virtual ~Plano_de_voo();
+};
+
+#endif /* PLANO_DE_VOO_H_ */
