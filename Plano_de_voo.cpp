6bf83d41c99273e88faa4aba8450eb07a612fc99
Pedro Borges <highvolt16@gmail.com>
2010-11-01 12:46:24 +0000

    Bug corregido no plano de voo

--- Plano_de_voo.cpp
+++ Plano_de_voo.cpp
@@ -6,14 +6,15 @@
  */
 
 #include "Plano_de_voo.h"
+using namespace std;
 
-Plano_de_voo::Plano_de_voo(int hora_partida,int min_partida, int dia_partida, int mes_partida, int ano_partida, int hora_chegada,int min_chegada, int dia_chegada, int mes_chegada, int ano_chegada, Companhia_aerea* companhia, string origem, string destino, Aviao* aviao, Companhia_aerea* companhia,int n_passageiros){
+
+Plano_de_voo::Plano_de_voo(int hora_partida,int min_partida, int dia_partida, int mes_partida, int ano_partida, int hora_chegada,int min_chegada, int dia_chegada, int mes_chegada, int ano_chegada, Companhia_aerea* companhia, string origem, string destino, Aviao* aviao){
 	this->partida.hora=hora_partida;
 	this->partida.min=min_partida;
 	this->partida.dia=dia_partida;
 	this->partida.mes=mes_partida;
 	this->partida.ano=ano_partida;
-
 }
 
 Plano_de_voo::~Plano_de_voo() {
--- Plano_de_voo.h
+++ Plano_de_voo.h
@@ -31,7 +31,7 @@ private:
 	int n_passageiros;
 
 public:
-	Plano_de_voo(int hora_partida,int min_partida, int dia_partida, int mes_partida, int ano_partida, int hora_chegada,int min_chegada, int dia_chegada, int mes_chegada, int ano_chegada, Companhia_aerea* companhia, string origem, string destino, Aviao* aviao, Companhia_aerea* companhia,int n_passageiros);
+	Plano_de_voo(int hora_partida,int min_partida, int dia_partida, int mes_partida, int ano_partida, int hora_chegada,int min_chegada, int dia_chegada, int mes_chegada, int ano_chegada, Companhia_aerea* companhia, string origem, string destino, Aviao* aviao);
 	virtual ~Plano_de_voo();
 };
 
