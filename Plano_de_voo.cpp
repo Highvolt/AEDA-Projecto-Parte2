36628cd99ebca42a6bfc3725d6996d30267de217
Pedro Borges <highvolt16@gmail.com>
2010-11-06 12:31:43 +0000

    doxygen

--- Plano_de_voo.cpp
+++ Plano_de_voo.cpp
@@ -43,6 +43,25 @@ void set_horas(int hora,int min, int dia, int mes, int ano, horas_data* dest){
 	}
 }
 
+/**
+ * class constructor that creates a flight  plan
+ *
+ * @param int hora_partida
+ * @param int min_partida
+ * @param int dia_partida
+ * @param int mes_partida
+ * @param int ano_partida
+ * @param int hora_chegada
+ * @param int min_chegada
+ * @param int dia_chegada
+ * @param int mes_chegada
+ * @param int ano_chegada
+ * @param Companhia_aerea* companhia
+ * @param string origem
+ * @param string destino
+ * @param Aviao* aviao
+ * @param int n_passageiros
+ */
 Plano_de_voo::Plano_de_voo(int hora_partida,int min_partida, int dia_partida, int mes_partida, int ano_partida, int hora_chegada,int min_chegada, int dia_chegada, int mes_chegada, int ano_chegada, Companhia_aerea* companhia, string origem, string destino, Aviao* aviao,int n_passageiros){
 	set_horas(hora_partida,min_partida,dia_partida,mes_partida,ano_partida,&partida);
 	set_horas(hora_chegada,min_chegada,dia_chegada,mes_chegada,ano_chegada,&chegada);
@@ -134,6 +153,11 @@ void Plano_de_voo::setPassageiros(int n_passageiros)
 	this->n_passageiros = n_passageiros;
 }
 
+/**
+ * Function that allow us to know if the flight plan is valid or not
+ * @name valid
+ * @return 0 if the flight plan is valid and 1 if it isn't
+ */
 bool Plano_de_voo::valid(){
 	if(this->partida.hora!=-1 && this->partida.min!=-1 && this->partida.dia!=-1 && this->partida.mes!=-1 && this->partida.ano!=-1 && this->chegada.hora!=-1 && this->chegada.min!=-1 && this->chegada.dia!=-1 && this->chegada.mes!=-1 && this->chegada.ano!=-1){
 		return true;
@@ -142,8 +166,10 @@ bool Plano_de_voo::valid(){
 }
 
 
-
-
+/**
+ * class destructor
+ * @name ~Plano_de_voo
+ */
 Plano_de_voo::~Plano_de_voo() {
 	// TODO Auto-generated destructor stub
 }
